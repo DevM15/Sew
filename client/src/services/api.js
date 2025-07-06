b3f965a0c38c66817403d2d6e82225d803f36469
DevM15 <devmistry1501@gmail.com>
2025-07-06 19:31:22 +0530

    first commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://sew-p1h7.onrender.com/api',
 });
 
 export const createRoom = async () => {
